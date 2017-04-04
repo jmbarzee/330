a4e7122fb878b19e957133f692fac2e5ec53d7b8
Jacob Barzee <jbarzee@qualtrics.com>
2017-04-03 18:19:00 -0600

    mix for new project

---
+++ center/lib/center.ex
@@ -0,0 +1,18 @@
+defmodule Center do
+  @moduledoc """
+  Documentation for Center.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Center.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ center/test/center_test.exs
@@ -0,0 +1,8 @@
+defmodule CenterTest do
+  use ExUnit.Case
+  doctest Center
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
